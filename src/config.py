98e239ba92ade9233cf5d28b03b2c945a8398a90
ok-oldking <ok-oldking@users.noreply.github.com>
2026-04-03 23:58:34 +0000

    Ignore ok_tasks and remove Abc.py

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 from ok import ConfigOption
 
-version = "v1.0.1"
+version = "v1.0.2"
 #不需要修改version, Github Action打包会自动修改
 
 key_config_option = ConfigOption('Game Hotkey Config', { #全局配置示例
