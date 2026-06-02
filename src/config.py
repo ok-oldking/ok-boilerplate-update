d524bc0b565ae824d7df3706424c09107f96e90b
ok-oldking <ok-oldking@users.noreply.github.com>
2026-06-02 17:13:30 +0000

    update mirrorchyan upload filename

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 from ok import ConfigOption
 
-version = "v1.0.20"
+version = "v1.0.21"
 #不需要修改version, Github Action打包会自动修改
 
 key_config_option = ConfigOption('Game Hotkey Config', { #全局配置示例
